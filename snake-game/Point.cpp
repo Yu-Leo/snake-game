060f5ac9dbfe54eb2bac76c8761257aff08058e2
Lev <Levayu22@gmail.com>
2021-06-29 19:56:12 +0300

    Cleared Point

--- snake-game/Point.cpp
+++ snake-game/Point.cpp
@@ -1,7 +1,5 @@
 #include "Point.h"
 
-Point::Point() {}
-
 Point::Point(int x, int y) {
     this->x = x;
     this->y = y;
@@ -9,10 +7,6 @@ Point::Point(int x, int y) {
 
 bool Point::operator==(const Point& other) {
     return this->x == other.x && this->y == other.y;
-}
-
-bool Point::operator<(const Point& other) {
-    return (this->y < other.y) || (this->y == other.y && this->x < other.x);
 }
 
 std::ostream& operator<< (std::ostream& out, const Point& point) {
