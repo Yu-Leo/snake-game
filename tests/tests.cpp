a0fdea1d1f4978098a3f7d928fec4d7d3bcc2368
Lev <Levayu22@gmail.com>
2021-07-02 19:20:26 +0300

    Create tests-project

---
+++ tests/tests.cpp
@@ -0,0 +1,12 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace tests {
+	TEST_CLASS(tests) {
+	public:
+		
+		TEST_METHOD(TestMethod1) {
+		}
+	};
+}
