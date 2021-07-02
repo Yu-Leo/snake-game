21ba79597f28c9784352daf037c09b76ece23403
Lev Yuvenskiy <levayu22@gmail.com>
2021-07-02 19:46:10 +0300

    Add test Apple

---
+++ tests/testApple.cpp
@@ -0,0 +1,24 @@
+#include "CppUnitTest.h"
+
+#include "../snake-game/Apple.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace tests {
+    TEST_CLASS(TestApple) {
+    public:
+        
+        TEST_METHOD(TestDefaultConstructor) {
+            Apple a;
+            Assert::IsTrue(a.get_coordinates() == Point());
+        }
+
+        TEST_METHOD(TestConstructor) {
+            Apple a1(Point(2, 3));
+            Assert::IsTrue(a1.get_coordinates() == Point(2, 3));
+
+            Apple a2(Point(18, 10));
+            Assert::IsTrue(a2.get_coordinates() == Point(18, 10));
+        }
+    };
+}
