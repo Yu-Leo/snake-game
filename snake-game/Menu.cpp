001b2008b7026cec2b400ba750145154d922c803
Lev <Levayu22@gmail.com>
2021-07-08 16:59:48 +0300

    Add different colors for active and inactive items of menu

--- snake-game/Menu.cpp
+++ snake-game/Menu.cpp
@@ -1,38 +1,53 @@
 #include "Menu.h"
 
-#include "Size.h"
-
-void Menu::draw_main_menu(sf::RenderWindow& window) {
-    const float menu_item_interval = 20;
-    const float horizontal_padding = 40;
-    const float vertical_padding = 30;
-
-    float current_munu_item_y = 0;
+Size Menu::get_background_size() {
+    float current_menu_item_y = 0;
     float menu_item_max_width = 0;
 
     for (int i = 0; i < this->main_menu_items.size(); i++) {
-        this->main_menu_items[i].setPosition(0, current_munu_item_y);
-        current_munu_item_y += this->main_menu_items[i].getLocalBounds().height + menu_item_interval;
+        current_menu_item_y += this->main_menu_items[i].getLocalBounds().height + this->ITEM_PADDING;
         menu_item_max_width = std::max(menu_item_max_width, this->main_menu_items[i].getLocalBounds().width);
     }
 
-    const Size menu(menu_item_max_width + horizontal_padding * 2,
-        current_munu_item_y - menu_item_interval + vertical_padding * 2);
+    return Size(menu_item_max_width + this->HORIZONTAL_PADDING * 2,
+        current_menu_item_y - this->ITEM_PADDING + this->VERTICAL_PADDING * 2);
 
-    const int menu_pos_x = (window.getSize().x - menu.width) / 2;
-    const int menu_pos_y = (window.getSize().y - menu.height) / 2;
+}
 
-    sf::RectangleShape menu_rect(sf::Vector2f(menu.width, menu.height));
-    menu_rect.setPosition(menu_pos_x, menu_pos_y);
+void Menu::draw_background(sf::RenderWindow& window, const Size &size, const Position &pos) {
+    sf::RectangleShape menu_rect(sf::Vector2f(size.width, size.height));
+    menu_rect.setPosition(pos.x, pos.y);
     menu_rect.setFillColor(this->BACKGROUND_COLOR);
     window.draw(menu_rect);
+}
 
+void Menu::draw_items(sf::RenderWindow& window, const Position& bg_pos) {
+
+    float current_y_pos = 0.0;
     for (int i = 0; i < this->main_menu_items.size(); i++) {
-        this->main_menu_items[i].move(menu_pos_x + horizontal_padding,
-            menu_pos_y + vertical_padding);
+        sf::Color fill_color = (this->current_item_index == i ?
+            this->ACTIVE_TEXT_COLOR : this->INACTIVE_TEXT_COLOR);
+
+        this->main_menu_items[i].setFillColor(fill_color);
+
+        this->main_menu_items[i].setPosition(bg_pos.x + this->HORIZONTAL_PADDING,
+            bg_pos.y + this->VERTICAL_PADDING + current_y_pos);
+
+        current_y_pos += this->main_menu_items[i].getLocalBounds().height +
+            this->ITEM_PADDING;
+
         window.draw(this->main_menu_items[i]);
     }
+}
 
+void Menu::draw_main_menu(sf::RenderWindow& window) {
+    Size background_size = this->get_background_size();
+    Position background_pos((window.getSize().x - background_size.width) / 2,
+        (window.getSize().y - background_size.height) / 2);
+
+    this->draw_background(window, background_size, background_pos);
+    
+    this->draw_items(window, background_pos);
 }
 
 void Menu::set_font_settings() {
@@ -43,6 +58,6 @@ void Menu::set_font_settings() {
         this->main_menu_items.back().setString(this->main_menu_items_text[i]);
         this->main_menu_items.back().setFont(this->font);
         this->main_menu_items.back().setCharacterSize(40);
-        this->main_menu_items.back().setFillColor(this->TEXT_COLOR);
+        this->main_menu_items.back().setFillColor(this->INACTIVE_TEXT_COLOR);
     }
 }
