e520a0576ef5750c0ccd06be54867e0760c27dcf
Lev Yuvenskiy <levayu22@gmail.com>
2021-07-09 10:01:51 +0300

    Add menu navigate sounds

--- snake-game/Sounds.cpp
+++ snake-game/Sounds.cpp
@@ -11,6 +11,9 @@ void Sounds::play(int sound_name) {
         case this->COLLISION_WITH_WALL:
             this->collision_with_wall.play();
             break;
+        case this->MENU_NAVIGATE:
+            this->menu_navigate.play();
+            break;
     }
 }
 
@@ -18,10 +21,14 @@ void Sounds::load_sound_buffers() {
     this->sound_buffers.ate_apple.loadFromFile("./sounds/ate_apple.wav");
     this->sound_buffers.collision_with_wall.loadFromFile("./sounds/collision_with_wall.wav");
     this->sound_buffers.collision_with_body.loadFromFile("./sounds/collision_with_body.wav");
+    this->sound_buffers.menu_navigate.loadFromFile("./sounds/menu_navigate.wav");
+
 }
 
 void Sounds::set_sound_buffers() {
     this->ate_apple.setBuffer(this->sound_buffers.ate_apple);
     this->collision_with_wall.setBuffer(this->sound_buffers.collision_with_wall);
     this->collision_with_body.setBuffer(this->sound_buffers.collision_with_body);
+    this->menu_navigate.setBuffer(this->sound_buffers.menu_navigate);
+
 }
