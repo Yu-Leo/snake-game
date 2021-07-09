29452e86bee32053ff5ac8ba041f3ae3bd307c02
Lev <Levayu22@gmail.com>
2021-07-09 12:15:55 +0300

    Refactoring menu control

--- snake-game/MainWindow.h
+++ snake-game/MainWindow.h
@@ -17,9 +17,7 @@ public:
 
     void event_handling(); // Processing window's events 
 
-    void handling_control(const sf::Event& event); // Processing controlling
-
-    void handling_menu_navigation(const sf::Event& event);
+    
 
     void one_iteration(); // One game iteration
 
@@ -28,6 +26,13 @@ public:
     void delay();
 
 private:
+    
+    enum class ActiveMenu {
+        MAIN,
+        PAUSE,
+        SETTINGS
+    };
+
     int speed = 2; // 0 - low, 4 - fast
 
     Size window_size;
@@ -52,8 +57,14 @@ private:
     
     sf::Text score_text;
     sf::Text game_over_text;
+    
+    std::vector<std::string> main_menu_items = { "Start new game", "Settings", "Quit" };
+    Menu main_menu = Menu(this->main_menu_items);
 
-    Menu menu;
+    std::vector<std::string> pause_menu_items = { "Resume game", "Settings", "Quit" };
+    Menu pause_menu = Menu(this->pause_menu_items);
+
+    ActiveMenu active_menu = ActiveMenu::MAIN;
 
     const sf::Color BACKGROUND_COLOR = sf::Color(0, 0, 0);
     
@@ -73,6 +84,18 @@ private:
 
     void draw_screen();
 
+    void handling_control(const sf::Event& event); // Processing controlling
+
+    void handling_menu_navigation(const sf::Event& event);
+
+    void menu_operations();
+
     void main_menu_operations();
+
+    void pause_menu_operations();
+
+    void next_menu_item();
+
+    void previous_menu_item();
 };
 
